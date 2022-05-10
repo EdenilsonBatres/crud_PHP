32f232065e8a4235b54ae663cbf7d1799aa24f99
Edenilson <edenilson.batres@gmail.com>
2022-05-09 21:24:28 -0600

    botones de editar y eliminar agregados

--- index.php
+++ index.php
@@ -10,11 +10,12 @@
                     <!-- <strong>Holy guacamole!</strong> You should check in on some of those fields below. -->
                     <?= $_SESSION['message'] ?>
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times</span>
+                        <span aria-hidden="true">&times</span>
                     </button>
                 </div>
                 <!-- //la funcion de unset elimina las variables de sesion -->
-            <?php session_unset(); } ?>
+            <?php session_unset();
+            } ?>
 
             <div class="card card-body">
                 <form action="save_task.php " method="POST">
@@ -31,7 +32,46 @@
             </div>
         </div>
         <div class="col-md-8">
+            <!-- //inicio del bloque de rellenado de tablas -->
+            <table class="table table-bordered">
+                <thead>
+                    <!-- cabecera de la tabla -->
+                    <tr>
+                        <!-- filas -->
+                        <th>Titulo</th>
+                        <th>Description</th>
+                        <th>Fecha</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <!-- // -->
+                <tbody>
+                    <!-- consulta a la base de datos -->
+                    <?php
+                    $query = "SELECT * FROM task";
+                    $resultado_tareas =  mysqli_query($con, $query);
+                    while ($row = mysqli_fetch_array($resultado_tareas)) {
+                    ?>
+                        <!--linea iterable con salida html -->
+                        <tr>
+                            <td><?php echo $row['title'] ?></td>
+                            <td><?php echo $row['description'] ?></td>
+                            <td><?php echo $row['created_at'] ?></td>
+                            <td>
+                                <a href="edit_task.php?id=<?php echo $row['id'] ?>" class="btn btn-secondary">
+                                    <i class="fas fa-marker"></i>
+                                </a>
+                                <a href="delete_task.php?id=<?php echo $row['id'] ?>" class="btn btn-danger">
+                                 <i class="fas fa-trash-alt"></i>
+                                </a>
+                            </td>
 
+                        </tr>
+                    <?php } ?>
+
+
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
